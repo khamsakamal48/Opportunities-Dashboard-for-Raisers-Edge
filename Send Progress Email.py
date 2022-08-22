ba62065ad9f8a75ff7b524bae562fbd8e71b6b1b
Kamal Hamza <khamsa.kamal48@gmail.com>
2022-08-22 18:58:51 +0530

    Initial version

---
+++ Send Progress Email.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python3
+
+import requests, os, json, glob, csv, psycopg2, sys, smtplib, ssl, imaplib, time, datetime, logging
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
+from jinja2 import Environment
+from datetime import datetime
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+from datetime import date
+
+# Printing the output to file for debugging
+sys.stdout = open('Process.log', 'w')
+
+# API Request strategy
+print("Setting API Request strategy")
+retry_strategy = Retry(
+total=3,
+status_forcelist=[429, 500, 502, 503, 504],
+allowed_methods=["HEAD", "GET", "OPTIONS"],
+backoff_factor=10
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+http = requests.Session()
+http.mount("https://", adapter)
+http.mount("http://", adapter)
+
+# Set current directory
+print("Setting current directory")
+os.chdir(os.getcwd())
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Retrieve contents from .env file
+DB_IP = os.getenv("DB_IP")
+DB_NAME = os.getenv("DB_NAME")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+RE_API_KEY = os.getenv("RE_API_KEY")
+MAIL_USERN = os.getenv("MAIL_USERN")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+IMAP_URL = os.getenv("IMAP_URL")
+IMAP_PORT = os.getenv("IMAP_PORT")
+SMTP_URL = os.getenv("SMTP_URL")
+SMTP_PORT = os.getenv("SMTP_PORT")
+ERROR_EMAILS_TO  = os.getenv("ERROR_EMAILS_TO")
+
+def connect_db():
+    global conn, cur
+    # PostgreSQL DB Connection
+    conn = psycopg2.connect(host=DB_IP, dbname=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD)
+
+    # Open connection
+    print("Creating connection with SQL database")
+    cur = conn.cursor()
+
+def disconnect_db():
+    # Close DB connection
+    if conn:
+        cur.close()
+        conn.close()
+    
+    # Close writing to Process.log
+    sys.stdout.close()
+    
+    exit()
+    
+def housekeeping():
+    # Housekeeping
+    multiple_files = glob.glob("*_RE_*.json")
+
+    # Iterate over the list of filepaths & remove each file.
+    print("Removing old files")
+    for each_file in multiple_files:
+        try:
+            os.remove(each_file)
+        except:
+            pass
+        
+    # Housekeeping
+    multiple_files = glob.glob("*.csv")
+
+    # Iterate over the list of filepaths & remove each file.
+    print("Removing old files")
+    for each_file in multiple_files:
+        try:
+            os.remove(each_file)
+        except:
+            pass
+
+def send_error_emails():
+    print("Sending email for an error")
+    
+    message = MIMEMultipart()
+    message["Subject"] = subject
+    message["From"] = MAIL_USERN
+    message["To"] = ERROR_EMAILS_TO
+
+    # Adding Reply-to header
+    message.add_header('reply-to', MAIL_USERN)
+        
+    TEMPLATE="""
+    <table style="background-color: #ffffff; border-color: #ffffff; width: auto; margin-left: auto; margin-right: auto;">
+    <tbody>
+    <tr style="height: 127px;">
+    <td style="background-color: #363636; width: 100%; text-align: center; vertical-align: middle; height: 127px;">&nbsp;
+    <h1><span style="color: #ffffff;">&nbsp;Raiser's Edge Automation: {{job_name}} Failed</span>&nbsp;</h1>
+    </td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="height: 18px; background-color: #ffffff; border-color: #ffffff;">&nbsp;</td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="width: 100%; height: 18px; background-color: #ffffff; border-color: #ffffff; text-align: center; vertical-align: middle;">&nbsp;<span style="color: #455362;">This is to notify you that execution of Auto-updating Alumni records has failed.</span>&nbsp;</td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="height: 18px; background-color: #ffffff; border-color: #ffffff;">&nbsp;</td>
+    </tr>
+    <tr style="height: 61px;">
+    <td style="width: 100%; background-color: #2f2f2f; height: 61px; text-align: center; vertical-align: middle;">
+    <h2><span style="color: #ffffff;">Job details:</span></h2>
+    </td>
+    </tr>
+    <tr style="height: 52px;">
+    <td style="height: 52px;">
+    <table style="background-color: #2f2f2f; width: 100%; margin-left: auto; margin-right: auto; height: 42px;">
+    <tbody>
+    <tr>
+    <td style="width: 50%; text-align: center; vertical-align: middle;">&nbsp;<span style="color: #ffffff;">Job :</span>&nbsp;</td>
+    <td style="background-color: #ff8e2d; width: 50%; text-align: center; vertical-align: middle;">&nbsp;{{job_name}}&nbsp;</td>
+    </tr>
+    <tr>
+    <td style="width: 50%; text-align: center; vertical-align: middle;">&nbsp;<span style="color: #ffffff;">Failed on :</span>&nbsp;</td>
+    <td style="background-color: #ff8e2d; width: 50%; text-align: center; vertical-align: middle;">&nbsp;{{current_time}}&nbsp;</td>
+    </tr>
+    </tbody>
+    </table>
+    </td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="height: 18px; background-color: #ffffff;">&nbsp;</td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="height: 18px; width: 100%; background-color: #ffffff; text-align: center; vertical-align: middle;">Below is the detailed error log,</td>
+    </tr>
+    <tr style="height: 217.34375px;">
+    <td style="height: 217.34375px; background-color: #f8f9f9; width: 100%; text-align: left; vertical-align: middle;">{{error_log_message}}</td>
+    </tr>
+    </tbody>
+    </table>
+    """
+    
+    # Create a text/html message from a rendered template
+    emailbody = MIMEText(
+        Environment().from_string(TEMPLATE).render(
+            job_name = subject,
+            current_time=datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            error_log_message = Argument
+        ), "html"
+    )
+    
+    # Add HTML parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(emailbody)
+    attach_file_to_email(message, 'Process.log')
+    emailcontent = message.as_string()
+    
+    # Create secure connection with server and send email
+    context = ssl._create_unverified_context()
+    with smtplib.SMTP_SSL(SMTP_URL, SMTP_PORT, context=context) as server:
+        server.login(MAIL_USERN, MAIL_PASSWORD)
+        server.sendmail(
+            MAIL_USERN, ERROR_EMAILS_TO, emailcontent
+        )
+
+    # Save copy of the sent email to sent items folder
+    with imaplib.IMAP4_SSL(IMAP_URL, IMAP_PORT) as imap:
+        imap.login(MAIL_USERN, MAIL_PASSWORD)
+        imap.append('Sent', '\\Seen', imaplib.Time2Internaldate(time.time()), emailcontent.encode('utf8'))
+        imap.logout()
+
+def attach_file_to_email(message, filename):
+    # Open the attachment file for reading in binary mode, and make it a MIMEApplication class
+    with open(filename, "rb") as f:
+        file_attachment = MIMEApplication(f.read())
+    # Add header/name to the attachments    
+    file_attachment.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+    # Attach the file to the message
+    message.attach(file_attachment)
+    
+def print_json(d):
+    print(json.dumps(d, indent=4))
+      
+def identify_current_quarter():
+    global current_quarter, current_quarter_end_date, previous_quarter_end_date
+    print("Identifying current quarter")
+    
+    current_month = int(datetime.now().strftime("%m"))
+    current_year = int(datetime.now().strftime("%Y"))
+    print(f"Current month: {current_month}")
+    print(f"Current year: {current_year}")
+    
+    # Current month between Apr and Jun
+    if current_month >= 4 and current_month <= 6:
+        current_quarter = "Q1"
+        current_quarter_end_date = datetime.strptime(f"30-06-{current_year}", "%d-%m-%Y").date()
+        previous_quarter_end_date = datetime.strptime(f"31-03-{current_year}", "%d-%m-%Y").date()
+    
+    # Current month between Jul and Sep
+    elif current_month >= 7 and current_month <= 9:
+        current_quarter = "Q2"
+        current_quarter_end_date = datetime.strptime(f"30-09-{current_year}", "%d-%m-%Y").date()
+        previous_quarter_end_date = datetime.strptime(f"30-06-{current_year}", "%d-%m-%Y").date()
+    
+    # Current month between Oct and Dec    
+    elif current_month >= 10 and current_month <= 12:
+        current_quarter = "Q3"
+        current_quarter_end_date = datetime.strptime(f"31-12-{current_year}", "%d-%m-%Y").date()
+        previous_quarter_end_date = datetime.strptime(f"30-09-{current_year}", "%d-%m-%Y").date()
+    
+    # Current month between Jan and Mar    
+    elif current_month >= 1 and current_month <= 3:
+        current_quarter = "Q4"
+        current_quarter_end_date = datetime.strptime(f"31-03-{current_year}", "%d-%m-%Y").date()
+        previous_quarter_end_date = datetime.strptime(f"31-12-{current_year - 1}", "%d-%m-%Y").date()
+        
+    print(f"Current Quarter: {current_quarter}")
+    print(f"Current Quarter End date: {current_quarter_end_date}")
+    print(f"Previous Quarter End date: {previous_quarter_end_date}")
+
+def get_corporate_pipeline_progress():
+    extract_sql = """
+    SELECT * from opportunity_list where date = '%s'
+    """
+    print(extract_sql)
+    # cur.execute(extract_sql, [query_date])
+    # result = list(cur.fetchall())
+    
+    # print(result)
+
+try:
+    # Connect to DB
+    connect_db()
+    
+    # Identify Current Quarter
+    identify_current_quarter()
+    
+    # Get data for Current quarter
+    while current_quarter_end_date:
+        query_date = current_quarter_end_date
+        get_corporate_pipeline_progress()
+
+except Exception as Argument:
+    subject = "Error while preparing opportunity pipeline progress from Raisers Edge"
+    print(subject)
+    send_error_emails()
+    
+finally:
+    # Do housekeeping
+    housekeeping()
+    
+    # Disconnect DB
+    disconnect_db()
