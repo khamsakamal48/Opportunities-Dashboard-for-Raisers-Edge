580cbab5f5cea170692046380648535411063c5f
Kamal Hamza <khamsa.kamal48@gmail.com>
2022-09-25 01:58:53 +0530

    Updated to capture date created of a constituent

--- Download Opportunities from RE.py
+++ Download Opportunities from RE.py
@@ -241,7 +241,7 @@ def get_constituent_from_re():
     
     # Request parameters for Blackbaud API request
     params = {
-        'fields': 'id, name, type',
+        'fields': 'id, name, type, date_added',
         'limit': '5000'
     }
     
@@ -269,7 +269,7 @@ def get_constituent_from_re():
             
     # Parse from JSON and write to CSV file
     # Header of CSV file
-    header = ['constituent_id', 'name', 'type']
+    header = ['constituent_id', 'name', 'type', 'date_added']
 
     with open('Constituents_in_RE.csv', 'w', encoding='UTF8') as csv_file:
         writer = csv.writer(csv_file, delimiter = ";")
@@ -293,7 +293,7 @@ def get_constituent_from_re():
             json_content = json.load(json_file)
 
             for results in json_content['value']:
-                data = (results['id'],results['name'].replace(";", ","),results['type'])
+                data = (results['id'],results['name'].replace(";", ","),results['type'],results['date_added'])
                 
                 with open('Constituents_in_RE.csv', 'a', encoding='UTF8') as csv_file:
                     writer = csv.writer(csv_file, delimiter = ";")
